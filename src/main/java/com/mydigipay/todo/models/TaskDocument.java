5cf36ea946b74b641239b739e5b12c6440f43003
omidsa1 <omidsaaedi@gmail.com>
2022-11-22 22:59:44 +0330

    Add builder to TaskDocument

--- src/main/java/com/mydigipay/todo/models/TaskDocument.java
+++ src/main/java/com/mydigipay/todo/models/TaskDocument.java
@@ -4,7 +4,6 @@ package com.mydigipay.todo.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 @Document(collection = "tasks")
 public class TaskDocument {
 
@@ -32,6 +31,7 @@ public class TaskDocument {
             this.owner = owner;
             this.assignee = assignee;
         }
+
 
         public String getId() {
             return id;
@@ -81,4 +81,48 @@ public class TaskDocument {
             this.assignee = assignee;
         }
 
+
+        public static class Builder {
+            private String id;
+            private String title;
+            private String description;
+            private String status;
+            private UserDocument owner;
+            private UserDocument assignee;
+
+            public Builder id(String id) {
+                this.id = id;
+                return this;
+            }
+
+            public Builder title(String title) {
+                this.title = title;
+                return this;
+            }
+
+            public Builder description(String description) {
+                this.description = description;
+                return this;
+            }
+
+            public Builder status(String status) {
+                this.status = status;
+                return this;
+            }
+
+            public Builder owner(UserDocument owner) {
+                this.owner = owner;
+                return this;
+            }
+
+            public Builder assignee(UserDocument assignee) {
+                this.assignee = assignee;
+                return this;
+            }
+
+            public TaskDocument build() {
+                return new TaskDocument(id, title, description, status, owner, assignee);
+            }
+
+        }
 }
